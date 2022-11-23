d47e5b18e2c0740b1026a2897d4bd0723ecae481
obirikan <obirikan020@gmail.com>
2022-11-23 12:57:03 +0000

    +++

---
+++ script.js
@@ -0,0 +1,15 @@
+let gh=document.getElementById('gh')
+let usd=document.getElementById('usd')
+
+let gh1=document.getElementById('gh1')
+let usd1=document.getElementById('usd1')
+
+function change(){
+let ghc=gh.value*0.069
+document.getElementById('usd').value=ghc
+}
+//
+function change2(){
+    let ghc=usd1.value*14.50
+    document.getElementById('gh1').value=ghc
+ }
